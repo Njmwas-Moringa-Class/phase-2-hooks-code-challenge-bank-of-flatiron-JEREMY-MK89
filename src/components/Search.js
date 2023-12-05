db77e7b9a2b4f6f4f60dbd63b2a58854f30e6dcf
moses <moses.kingi@moringaschool.com>
2023-12-05 13:41:57 +0300

    bank of flatiron

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,17 @@
+// Search.js
 import React from "react";
 
-function Search() {
+function Search({ handleSearch }) {
+  function handleChange(event) {
+    handleSearch(event.target.value);
+  };
+
   return (
     <div className="ui large fluid icon input">
       <input
         type="text"
         placeholder="Search your Recent Transactions"
-        onChange={() => console.log("Searching...")}
+        onChange={handleChange}
       />
       <i className="circular search link icon"></i>
     </div>
